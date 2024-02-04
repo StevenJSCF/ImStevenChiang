51a47566638a4d184c63c3641ecd73cb32835676
Joshua Chiang <stevenchiang09@gmail.com>
2024-02-04 17:55:15 -0600

    Finished uploading the project

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.querySelector('.flip-btn').addEventListener('click', function() {
+    document.querySelector('.flip-container').classList.toggle('flipped');
+  });
